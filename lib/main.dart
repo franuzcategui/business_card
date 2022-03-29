f86a7bd2ee36a44ae43632cd402d9944776816f4
Francisco Uzcategui <franuzcategui95@gmail.com>
2022-03-29 14:04:08 -0600

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,103 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              backgroundImage: AssetImage('images/profile.png'),
+              radius: 60.00,
+            ),
+            Text(
+              'Francisco Uzcategui',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                color: Colors.white,
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Flutterist',
+              style: TextStyle(
+                fontFamily: 'Source Sans Pro',
+                color: Colors.white,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.5,
+              ),
+            ),
+            SizedBox(
+                width: 300.0,
+                child: Divider(
+                  color: Colors.white,
+                )),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Padding(
+                  padding: const EdgeInsets.all(0.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      size: 30,
+                      color: Colors.blueGrey,
+                    ),
+                    title: Text(
+                      '6174857996',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'Telephone',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 10,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.blueGrey,
+                ),
+                title: Text(
+                  'franuzcategui95@gmail.com',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                subtitle: Text(
+                  'email',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 10,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            )
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
